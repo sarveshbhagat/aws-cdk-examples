68b6869ce22cd05731af97c79e70cabd4519c71c
sarbha <sarbha@amazon.com>
2020-01-21 22:35:55 -0500

    consumer stack

---
+++ python/dynamodb-lambda/dynamodb_lambda/consumer.py
@@ -0,0 +1,18 @@
+from aws_cdk import (
+    core,
+    aws_lambda,
+    aws_dynamodb,
+    aws_events,
+    aws_events_targets,
+)
+
+
+class ConsumerStack(core.Stack):
+    def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
+
+        # create consumer lambda function
+        consumer_lambda = aws_lambda.Function(self, "consumer_lambda_function",
+                                              runtime=aws_lambda.Runtime.PYTHON_3_6,
+                                              handler="lambda_function.lambda_handler",
+                                              code=aws_lambda.Code.asset("./lambda/consumer"))
